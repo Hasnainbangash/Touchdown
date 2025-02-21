faaae2fcea3a39bd7d0f9a554c1a067b354187ed
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-21 17:24:22 +0500

    Made the template  constant file along with the comments in it

---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,18 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Elexoft on 21/02/2025.
+//
+
+import SwiftUI
+
+// DATA
+// COLOR
+// LAYOUT
+// UX
+// API
+// IMAGE
+// FONT
+// STRING
+// MISC
