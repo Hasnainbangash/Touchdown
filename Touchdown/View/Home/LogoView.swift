e5552f0c546559182b191de8271e72dfc97439e0
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-21 17:57:16 +0500

    Logo view of the navigation bar view work done

---
+++ Touchdown/View/Home/LogoView.swift
@@ -0,0 +1,35 @@
+//
+//  LogoView.swift
+//  Touchdown
+//
+//  Created by Elexoft on 21/02/2025.
+//
+
+import SwiftUI
+
+struct LogoView: View {
+    var body: some View {
+        HStack(spacing: 4) {
+            Text("Touch".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+            
+            Image("logo-dark")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 30, alignment: .center)
+            
+            Text("Down".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+        } //: HSTACK
+    }
+}
+
+#Preview {
+    LogoView()
+        .previewLayout(.sizeThatFits)
+        .padding()
+}
