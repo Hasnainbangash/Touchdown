ffe07e08f68dc6003546d8f24f38862ba2111f9a
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-24 11:16:54 +0500

    Grid layout contnt get created

--- Touchdown/Utility/Constant.swift
+++ Touchdown/Utility/Constant.swift
@@ -18,6 +18,13 @@ let colorBackground: Color = Color("ColorBackground")
 let colorGray: Color = Color(UIColor.systemGray4)
 
 // LAYOUT
+
+let columnSpacing: CGFloat = 10
+let rowSpacing: CGFloat = 10
+var gridLayout: [GridItem] {
+    return Array(repeating: GridItem(.flexible(), spacing: rowSpacing), count: 2)
+}
+
 // UX
 // API
 // IMAGE
