89259271a5a6f8bb9c295b4f14bac54e8e4ee9e1
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-24 12:39:41 +0500

    Brand data model is crated and set it in the constrants

---
+++ Touchdown/Model/BrandModel.swift
@@ -0,0 +1,13 @@
+//
+//  BrandModel.swift
+//  Touchdown
+//
+//  Created by Elexoft on 24/02/2025.
+//
+
+import Foundation
+
+struct Brand: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
