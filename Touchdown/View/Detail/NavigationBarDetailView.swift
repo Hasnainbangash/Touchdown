fb01726bfe0ebe4eeb787a7a5fa3d56fe0ce3125
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-24 18:03:13 +0500

    back button in the navigation bar detail view wrok done and when click on the back button than the seleted product get null and the showing product will get false which means that it will show the main page

--- Touchdown/View/Detail/NavigationBarDetailView.swift
+++ Touchdown/View/Detail/NavigationBarDetailView.swift
@@ -10,11 +10,18 @@ import SwiftUI
 struct NavigationBarDetailView: View {
     // MARK: - PROPERTIES
     
+    @EnvironmentObject var shop: Shop
+    
     // MARK: - BODY
     
     var body: some View {
         HStack {
-            Button(action: {}, label: {
+            Button(action: {
+                withAnimation(.easeIn) {
+                    shop.selectedProduct = nil
+                    shop.showingProduct = false
+                }
+            }, label: {
                 Image(systemName: "chevron.left")
                     .font(.title)
                     .foregroundColor(.white)
@@ -35,6 +42,7 @@ struct NavigationBarDetailView: View {
 
 #Preview {
     NavigationBarDetailView()
+        .environmentObject(Shop())
         .previewLayout(.sizeThatFits)
         .padding()
         .background(Color.gray)
