1a86ade64db637cdaf6786712ab6dab2d41343a6
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-24 13:09:35 +0500

    New file is crated is product deatil view screen and add the comments of the tings in the screen

---
+++ Touchdown/App/ProductDetailView.swift
@@ -0,0 +1,34 @@
+//
+//  ProductDetailView.swift
+//  Touchdown
+//
+//  Created by Elexoft on 24/02/2025.
+//
+
+import SwiftUI
+
+struct ProductDetailView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 5) {
+            // NAVBAR
+            // HEADER
+            // DETAIL TOP PART
+            // DETAIL BOTTOM PART
+            // RATINGS + SIZES
+            // DESCRIPTIONS
+            // QUANTITY + FAVOURITE
+            // ADD TO CART
+        } //: VSTACK
+    }
+}
+
+// MARK: - PREVIEW
+
+#Preview {
+    ProductDetailView()
+        .previewLayout(.fixed(width: 375, height: 812))
+}
